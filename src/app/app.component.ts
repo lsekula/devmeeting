fa2d5185fc56b5b0592f8d372040d966b0a7c1a6
Norbert Bylak <norbert.bylak@webcrafters.pl>
2017-07-22 11:08:42 +0200

    products list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent
+
+{
+
+  productTitle: string = 'Produkt testowy';
+
+  products: {} = [
+  {id: 1, name: 'Testowy', price: '121', productImage: '', promoted: true},
+  {id: 2, name: 'Testowy', price: '321', productImage: '', promoted: false},
+  {id: 3, name: 'Testowy', price: '311', productImage: '', promoted: false},
+  {id: 4, name: 'Testowy', price: '125', productImage: '', promoted: false},
+  {id: 5, name: 'Testowy', price: '221', productImage: '', promoted: false}
+  ];
+
 }
