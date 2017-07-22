26fc28e30dd24eca9ac28e9ce9a5354600c3ae54
Luc Sekula <l.sekula@poczta.pl>
2017-07-22 11:51:09 +0200

    product.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Product} from './app.product.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app';
+  
+  public list: Array<Product> = [
+      new Product('ferst', 1),
+      new Product('second', 2),
+      new Product('third', 3)
+  ];
+
+  constructor() {
+      this.list[0].setPromoted();
+  }
 }
---
+++ src/app/app.product.component.ts
@@ -0,0 +1,31 @@
+import {Component, Input} from '@angular/core';
+
+export class Product {
+    name: string;
+    price: number;
+    promoted: boolean;
+
+    constructor(name: string, price: number) {
+        this.name = name;
+        this.price = price;
+        this.promoted = false;
+    }
+
+    setPromoted() {
+        this.promoted = true;
+    }
+}
+
+@Component({
+    selector: 'product',
+    template: `
+        <p>name: {{ item.name }}, price: {{ item.price }}
+            <span *ngIf="item.promoted">, product is promoted</span>
+        </p>
+    `
+})
+export class ProductComponent {
+    // To receive data we use Input annotation
+    public item;
+}
+
